9d666e5e3f90ccc6e903cd8ed9da1119bf726193
Gustavo Lima <adm.gustavo.lima@gmail.com>
2025-03-20 16:28:27 -0300

    Incluindo requirements

---
+++ python-data-science/Operadores de comparação em Python.py
@@ -0,0 +1,11 @@
+# Operadores de comparação em Python
+
+# Operador   | Operação
+# -----------------------------------------------
+# ==         | True se os valores em ambos lados forem iguais
+# !=         | True se os valores em ambos lados forem diferentes
+# <>         | True se os valores em ambos lados forem diferentes (OBS: Esse operador é obsoleto, use "!=")
+# >          | True se o valor da esquerda for maior que o valor da direita
+# <          | True se o valor da esquerda for menor que o valor da direita
+# >=         | True se o valor da esquerda for maior ou igual ao valor da direita
+# <=         | True se o valor da esquerda for menor ou igual ao valor da direita
